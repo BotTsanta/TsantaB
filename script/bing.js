6ede52e9175d99478cfc1d38bf072f1d84e29f0c
tstbt <158824950+BotTsanta@users.noreply.github.com>
2024-04-04 14:12:15 +0300

    Update bing.js

--- script/bing.js
+++ script/bing.js
@@ -2,8 +2,8 @@ const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
 
-const KievRPSSecAuth = "FAByBBRaTOJILtFsMkpLVWSG6AN6C/svRwNmAAAEgAAACPdEWecX9G/OMAQ2nuMaOCul+CDMdOHAACpuCIh5U8btXkpIavLSnFJmvhw7unH4LdSog9rOSNi3ud5GDzQzFaN/0zpUBdAaiDZ+moU8XXTncvvrXgU8Poabcue80cT6H1dW6S3OPJAPQi2+dcwdWsdwlRS/PItdgb8/qytPGyoAF+ERFSVb6iR5ocsV52kN+BqjAGfUEYHS2FR1i4cNanwgZxH+lpxqfkKY5mgHmWlPbDBGWN/4nWbcVvwRZq4YnAMHj1lup5EEKwES5bHMfYvpQ62bqA7k1t8NbmDqoEj08Ap1StgXadTDqoqoQSyNP83JXXtGlBMqrPgquyqD50SOFCYDqyv9U3X1PzTEVTdW3KAqcYAsHv6DScT//n7B2DksZYv9Bsg0BMYPsIp5DPjjTrtGCTNagaufXHkaYrOyDq40fbDIyhrh4wJQoNmTPmloNFl9OqL/5ZWuPgOafC7+yDFhmqGOWImaNpU2WejvGvD3P9IZN4Z1f36XSJJIO4pZKAhhwWFkefa3LN8fAYnsJ+WkjDSXAGTuwN6NnW9wzGWnFaD4ojKunjPazYt5qLtlk06PYoqDE6V/MSKxIU2+GVg7gMtBNDMwEKD+pMHB0jVjzx0SyeEYc8m6K/ThfrBGXdwzQWsEiMxP5cXAVLClGDPXRI2h2W6B/n9UGPi8utG7Sz/dV1QaKEtyJPohuO7Onk42jLZ0/EabwpJ7jwaiSYv2AIW+GKO8wRPRlt8nOSZxqCPDKOVgn4f9VoaudyjRPAM7KoiGJ7PhXgMYoqk6ZvmLpcPaWNoH3p7TbKgkO0fd811/3weWZ4dicF4bazE2++QD+wKhXqjiHJZrhJOEOTB1GpZ/wnXSf0pY7SG+qyJcHTr6PJ3l+yv0KKVPCzuv+kg3566u7EzAkZPy7N9bpljQ0UcjloH7FLz2MPo0ZocPvEbQohHE8PFf+0fh7GbtjzHoyfqjQBVeNfQL7s7VSEdOOoluAirLoZeBFWA+JVorvWXpRi/jpTMk45DX1TqZJAVWCWdn6d2yddmZ03CoyWR0xqfYqD69LCOxXBTdz54SsAmPZ+vjLByqlSakKFFSR5WBsJSdY7Hw0uBXunyHBJD0ERh5/igQYP5JwdbWbWvToeVFChimOSJ0iK8r3cEzk4yxiA+JD2KhbR/9TkW5yLHrn/NVvDL9nLSWUw2wWLFFU1iMAiL9E3SVjtw1odHJYhr+7JLBRqp2tBxb1DRiFftu/XyN2oXKLxpX7RzNFH6CD4ywgQ2q4aiQwKeOfvklWovQ/3LyrQn08/mVZ+cn6cMCzUAYMdzYT1OgfhLkYMy0w+zXxINAPkkNZD2ZKgWeJPMW/uBY1DHahr+DSX/vuh1WH6ABAJFl+Hn71BGI+AbjZA0uCqDOKQLfwBGBYsGDBXRfxlKMCf+QnGnPgjQgPXGA820kWxK8FACZ1lFF7mxJUEOcTFrjijsYKfwWLw==";
-const _U = "1blK93hKnYixhtwAtPdb3D8pFjTaKhmfhX5Z5ZE9dk-moahnidxLq97WCyxPXOsrZRrXOkAzyFh4LLCNUHgOHZJLpfF0hM23IrCUkYktZIv7xLUTHSeMHliuqQiWoWboYEgCvR5P1EAFpsFRhEo5l3gNwsFwfSFrzcxHp7roT6rH_4UuiMhvDo3S99XpinYvIA5Dmlu-mtzTyuvRb3J3kn5ALdabtwZDLhy9mr9JMVzE";
+const KievRPSSecAuth = "FAByBBRaTOJILtFsMkpLVWSG6AN6C/svRwNmAAAEgAAACEMcQOL4Cx20MATxQ9w4RUoj9c6udjcGWkwBzSqwnMW5jlcWEPaUALxs8bwfDODOEYc7jx6F+LeUTP+W2oISHZjhzb+kd0s9Qptl6Lv+J3GpSTStfDq44oMklokGqSZzUIrObpJQiO7jvwVNRuzPCOE1TyCN7pbhdi/IHYod90ekKGd5673Xz79AwDWrYDWmZXv3JYsWN35dxtzBwCUSy3Luf5mw2Ia7n+JR30r+B4aswwylFr6nugAaa/qFEgLdNLflrPxcbM5i5OVPcNyZ5Ruhf1hqz/1ow6g/bKuvzrPXnmACgN2iQjRHygeT2RzRuYYzCL9LORXndB3AbRJk6Mi8olqJJJ8tUwIbiDaMqO1QQnSMnJTipRCsbXI/DmapEO9bbdc1CgpwcjiOzxUug47ga8vutZDwcLNK9tsAD651WiFDdIXg8Twjhc5kzB29WmYMBJAnS70tklmVuRpjSlC+ydNXk5F0vofjpQjxrQvx4mHK8LcIaOAT5Q61pPtS+4GAKywcRg0M0tdOWSc3AJgeucGGFolkBn9xDkAbhHP05V6VOwFmwxY81YOqrh560I6hy3NY33K5rMWxp4QsAhm3/UdCI0ycIUAl5F0PiXfcSj4IuI+kHoIl1+ZWqzOEjjCw1hEHqfCLHi2Cn0Cj2q9gPhMSrh9maBJsWiISXG6cBEnQ5Ki0fPVeK0cMQuAGh4Rfp/6+8vjsk0bMWxsQGRk6vOp8teOHmlsoMe43OxgNyk47ei10uOMRDUuAcswwaLC8BPWNmUrgseUcK6x6aLS4Jix3FhcYXR55HYSexh2+XAGwIQ48SEIg6mXIjzyX8eJ0VSEUAsMDTqPKU3MxEXcgFDE62vNHmiRfOhIKATpw6oH93g/O3rAzj4Jxgy9EKPgLcEd2xYqlZN6EyGTGPUfytiik4kL++eS4+pfXYl8zk9wVBlxgWqnp3BaVuZlBd6G0yJeUGknwlRubb7+elidOSCcs9Dvu7qFxtRseK+5o4pwBs2hnx/UHf1rMmWS09DpbIyC+ap7jbQWLNO56mTCmPeHBdIdO+6/hyvr2g5FCsztQ6UpdaA2AMdu2OT3hSjVRBIW0OkBg68bcHF6OtIPGSxkz1MeUQ02iYMYseQsa+TAYRt9sBRmecCVLdYnYPCfpZ4IJrSeJBBq3uZIlDXnsct+NvpYWC2/URubBKIYJstjFATv7OxrsctXz34JXL6UtHrzyTiZEuCQHO7W+pO0ZrF74uvS6P+vvSeiPnhN1+XpCPfXqnWWMoxnJvOq4DRXPxhJiTZNDZRsDhiW9Y/HN9NvGdqjVTFmNJz/Kq96R9X3dqC2qLKeqnNfxJFwmtHT9/5aftfEUyXuO3Oz6MYvezWnAzsdwKyBgo6xYigLttwR6y6lJjOX3AnGkqcIbpqdfO4Vvfu7PVVKwMMZYZhGZaBWeZZ7yA8iPFAA08Qqz3kH3CMZyQrppGcf0XoTNYg==";
+const _U = "1-lFEmAhZ_cMUVWxQ_8p5x186Gwb7Q9XmRQoKa-_YT9SyuJ7YIhOGH9vVgP6NyFZ0sXTmvugV_WaDAmoj0ZSG8oTpVqe4YKQiRETkuOEgPlBrSW_TQlEIzVRu-jmKEq9YOBZ2E3tYjYdONDJrA2G8QeBzjrwH3ZeauRY33KBXUcFqAKOPlbnk8Sqmf7pooGW1YzkDJ8QV3wbmc6g6NIQZsMFySjHu79v54Bkymm68RfA";
 
 module.exports = {
   config: {
@@ -44,11 +44,11 @@ module.exports = {
     try {
       api.sendMessage("⏳ | TsantaBot et Bing sont en train d'imaginer votre textes... (⏰ Attendez..)\n\n_______________\n 🆓️ : Disponible chaque 5min\n 🌐 : bit.ly/tsantabot", event.threadID, event.messageID); // Added message here
 
-      const res = await axios.get(`https://api-dalle-gen.onrender.com/dalle3?auth_cookie_U=${_U}&auth_cookie_KievRPSSecAuth=${KievRPSSecAuth}&prompt=${encodeURIComponent(keySearchs)}`);
+      const res = await axios.get(`https://apis-dalle-gen.onrender.com/dalle3?auth_cookie_U=${_U}&auth_cookie_KievRPSSecAuth=${KievRPSSecAuth}&prompt=${encodeURIComponent(keySearchs)}`);
       const data = res.data.results.images;
 
       if (!data || data.length === 0) {
-        api.sendMessage("On ne sait pas d'imaginer votre textes. Essayez un autre. ", event.threadID, event.messageID);
+        api.sendMessage("On ne sait pas imaginer votre textes. Essayez un autre. ", event.threadID, event.messageID);
         return;
       }
 
